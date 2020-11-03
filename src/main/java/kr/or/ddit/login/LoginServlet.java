48689f2226e76a14882946c6abf11c00b7de1058
yrna <yelin968@gmail.com>
2020-11-03 15:18:07 +0900

    스프링 MVC  . dispatcher servlet 설정

---
+++ src/main/java/kr/or/ddit/login/LoginServlet.java
@@ -0,0 +1,44 @@
+package kr.or.ddit.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginServlet.class);
+
+	// Login화면을 클라이언트에게 응답으로 생성
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		logger.debug("loginServlet doGet");
+		logger.debug("UNT_CD parameter :{} ", req.getParameter("UNT_CD"));
+		
+		req.getRequestDispatcher("/login.jsp").forward(req, resp);
+		req.getServletContext().getNamedDispatcher("default").forward(req, resp);
+	}
+	
+	// 매번 servlet을 호출할때마다 실행 시키지 않고 한번만 생성시키기 위해 init()안에 생성시켰다.
+	@Override
+	public void init() throws ServletException {
+		
+	}
+	
+   
+	
+
+
+}
--- src/main/java/kr/or/ddit/person/model/Person.java
+++ src/main/java/kr/or/ddit/person/model/Person.java
@@ -1,10 +1,19 @@
 package kr.or.ddit.person.model;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import kr.or.ddit.board.repository.BoardRepositoryI;
 
 public class Person {
 	private int age;
 	private BoardRepositoryI boardRepository;
+	
+	// 개발을 할때 포맷이 달라져야한다고 해도 상관없다. -> 자유도 증가
+	// 주입하려는 필드마다 선언을 해줘야하는 단점
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date birthdate;
 	
 	public int getAge() {
 		return age;
@@ -18,11 +27,19 @@ public class Person {
 	public void setBoardRepository(BoardRepositoryI boardRepository) {
 		this.boardRepository = boardRepository;
 	}
-	@Override
-	public String toString() {
-		return "Person [age=" + age + ", boardRepository=" + boardRepository + "]";
+	
+	public Date getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
 	}
 	
+	
+	@Override
+	public String toString() {
+		return "Person [age=" + age + ", boardRepository=" + boardRepository + ", birthdate=" + birthdate + "]";
+	}
 	public Person() {
 
 	}
